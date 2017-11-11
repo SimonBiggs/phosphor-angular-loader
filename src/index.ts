b99cdaad2f6bb1bdfc58b00fdd8daef25defc662
SimonBiggs <mail@simonbiggs.net>
2017-11-11 14:42:44 +1100

    update to apache license, apply license header

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
 /*
-Bootstraps a root Angular component into a JupyterLab Widget.
+Copyright 2017 Simon Biggs
 
-Key properties on the AngularWidget are `componentInstance` and
-`componentReady`.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-Once the componentReady promise resolves then the Angular component is
-accessible as componentInstance.
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 
-import './polyfills';
+/**
+ * Bootstraps a root Angular component into a JupyterLab Widget.
+ * 
+ * Key properties on the AngularWidget are `componentInstance` and 
+ * `componentReady`.
+ * 
+ * Once the componentReady promise resolves then the Angular component is 
+ * accessible as `componentInstance`.
+ */
+
+// Polyfills
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
 
 import {
   Widget
